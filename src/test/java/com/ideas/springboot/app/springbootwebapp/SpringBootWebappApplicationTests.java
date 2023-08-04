cbd958b0e2f813158dc5995b81bbe0df5b437daa
Felipe Herman <j_felipe05@hotmail.com>
2023-08-04 01:42:00 -0500

    añadiendo todo sobre las anotaciones, controladores, vistas, directivas, paso de datos, etc.

---
+++ src/test/java/com/ideas/springboot/app/springbootwebapp/SpringBootWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ideas.springboot.app.springbootwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
